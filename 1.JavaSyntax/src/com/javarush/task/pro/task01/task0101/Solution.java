b9747c425e49e4e12824dd326fc9d5c12c666bb4
nikitos198 <chupik2@gmail.com>
2022-04-19 17:08:47 +0300

    Add Solution from JavaSyntax task 0101

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0101/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task01.task0101;
+
+/* 
+Первая программа
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Диего + Амиго = Друзья");
+        //напишите тут ваш код
+    }
+}

671c77ef4550bdcc60eb34c8d34c66ca1174adc0
nikitos198 <chupik2@gmail.com>
2022-04-19 18:17:22 +0300

    Del comm

--- 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
@@ -1,9 +1,5 @@
 package com.javarush.task.task01.task0101;
 
-/* 
-Я думаю, быть программистом круто
-*/
-
 public class Solution {
     public static void main(String[] args) {
         System.out.println("Я думаю, быть программистом круто");

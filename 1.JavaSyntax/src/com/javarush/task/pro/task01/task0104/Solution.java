fcd09c0a7c4d8c64f896312faf77da453ce21ac7
nikitos198 <chupik2@gmail.com>
2022-04-20 12:58:19 +0300

    Fix

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0104/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task01.task0104;
+
+/* 
+Починить систему уборки города
+*/
+//fix
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Уборка города началась");
+//        System.out.println("убираем людей");
+//        System.out.println("убираем животных");
+        System.out.println("убираем мусор");
+        System.out.println("убираем листья");
+//        System.out.println("убираем птиц");
+        System.out.println("уборка города закончилась");
+    }
+}

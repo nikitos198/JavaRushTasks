79483c4d300a969ba3518cfebf5c550499c5a733
nikitos198 <chupik2@gmail.com>
2022-04-20 12:18:23 +0300

    Deleted commit

--- 1.JavaSyntax/src/com/javarush/task/pro/task01/task0103/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0103/Solution.java
@@ -1,9 +1,5 @@
 package com.javarush.task.pro.task01.task0103;
 
-/* 
-Сбросить Диего и сверхсекретный груз
-*/
-
 public class Solution {
     public static void main(String[] args) {
         String drop = "Сбрасываем";

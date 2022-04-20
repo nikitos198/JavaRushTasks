f8017e5053b0290ab24fbab18548e73918d2b54a
nikitos198 <chupik2@gmail.com>
2022-04-20 12:27:40 +0300

    Deleted commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task01.task0103;
+
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 3126;
+        int b = 8;
+        System.out.println(a - b);
+    }
+}

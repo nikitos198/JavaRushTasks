a389208262f37174bbd3e006dff15070adc06d61
nikitos198 <chupik2@gmail.com>
2022-04-21 14:52:49 +0300

    Assign Variable Values

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int intValue = 10, numericValue = 15, size = 20, number = 25;
+
+    }
+}

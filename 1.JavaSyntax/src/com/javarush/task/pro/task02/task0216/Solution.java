d6d7b62b00eaa6cacdd63aa0fd48392726b0cf7a
nikitos198 <chupik2@gmail.com>
2022-04-21 15:52:34 +0300

    Declaring variables

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0216/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task02.task0216;
+
+/* 
+Генератор позитивных новостей
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "Ваня", date = "21 января", time = "в 15 часов", adjective = "\"MegaPC\"", noun =",", verb = "обрабатывает";
+        System.out.println("Новости:");
+        System.out.println(name + " " + date + " " + time + " изобрел " + adjective + " " + noun + " который " + verb + " террабайты данных в минуту.");
+    }
+}

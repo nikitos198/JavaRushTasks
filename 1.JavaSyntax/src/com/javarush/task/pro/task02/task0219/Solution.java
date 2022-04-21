15e863f7c20ca972137c38842b84f5e801676db3
nikitos198 <chupik2@gmail.com>
2022-04-21 14:49:52 +0300

    Assign Variable Values

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task02.task0219;
+
+public class Solution {
+    public static void main(String[] args) {
+        int id = 5, age = 10;
+        String documents = "documents", behavior = "behavior";
+
+    }
+}

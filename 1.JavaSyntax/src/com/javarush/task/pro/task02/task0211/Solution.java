dd98fd290b67f8319c23ae1fe91483e623f87bba
nikitos198 <chupik2@gmail.com>
2022-04-21 17:37:13 +0300

    gets the length of the string

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0211/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task02.task0211;
+
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String emptyString = "";
+
+        //напишите тут ваш код
+        System.out.println(emptyString.length());
+        System.out.println("Gomu Gomu no Bazooka!".length());
+        System.out.println((emptyString + 2 + 2 + "22").length());
+    }
+}

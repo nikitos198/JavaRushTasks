422d0c73f5da948bb962023c1ccce36ab6b90b9b
nikitos198 <chupik2@gmail.com>
2022-04-21 15:12:50 +0300

    Calculation of meters

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0202/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task02.task0202;
+
+/* 
+Полнометражная картина
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int centimeters = 243;
+
+        int meters = 100;
+        meters = centimeters / meters;
+
+        System.out.println(meters);
+    }
+}

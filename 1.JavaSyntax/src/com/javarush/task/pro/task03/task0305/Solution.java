37b0a5f9d01aa1b1e6c07e71d8dd575e9ee21ccf
nikitos198 <chupik2@gmail.com>
2022-04-24 15:36:40 +0300

    Add Solution from JavaSyntax task 0305

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0305/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task03.task0305;
+
+import java.util.Scanner;
+
+
+
+public class Solution {
+    public static void main(String[] args) {
+      Scanner console = new Scanner(System.in);
+      int a = console.nextInt();
+      int b = console.nextInt();
+      int c = console.nextInt();
+      if (a == b && b == c && a == c) {
+          System.out.println(a + " " + b + " " + c);
+      } else if (a == b) {
+          System.out.println(a + " " + b);          
+      } else if (a == c) {
+          System.out.println(a + " " + c);
+      } else if (b == c) {
+          System.out.println(b + " " +c);
+      }
+    }
+}

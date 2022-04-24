a155a587a75d1fec4f2e307412bd12d21f10d8ca
nikitos198 <chupik2@gmail.com>
2022-04-24 18:58:16 +0300

    Add Solution from JavaSyntax task 0309

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0309/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0309/Solution.java
@@ -2,10 +2,6 @@ package com.javarush.task.pro.task03.task0309;
 
 import java.util.Scanner;
 
-/* 
-Ищем максимум
-*/
-
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);

2dfe2d1fcf13bddd843e03b6b4381d1f7b0c83c0
nikitos198 <chupik2@gmail.com>
2022-04-25 18:07:00 +0300

    Add Solution from JavaSyntax task 0409

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0409/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0409/Solution.java
@@ -2,10 +2,6 @@ package com.javarush.task.pro.task04.task0409;
 
 import java.util.Scanner;
 
-/* 
-Минимум из введенных чисел
-*/
-
 public class Solution {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);

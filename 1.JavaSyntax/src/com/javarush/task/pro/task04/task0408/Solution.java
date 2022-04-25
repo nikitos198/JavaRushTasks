b144d582b2a28c45a885fd8d6107bbe375f4caf8
nikitos198 <chupik2@gmail.com>
2022-04-25 17:56:23 +0300

    Add Solution from JavaSyntax task 0408

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0408/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task04.task0408;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        int max = Integer.MIN_VALUE;
+        while (console.hasNextInt()) {
+            int x = console.nextInt();
+            if (x % 2 == 0 && x > max) {
+                max = x;
+            }
+        }
+        System.out.println(max);
+    }
+}

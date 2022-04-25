68ddb6c24f022eb8114617f464b2b4972eb381f2
nikitos198 <chupik2@gmail.com>
2022-04-25 13:13:53 +0300

    Add Solution from JavaSyntax task 0401

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task04.task0401;
+
+public class Solution {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+        int n = 0;
+        while (n < 100)
+        {
+            System.out.println(quote);
+            n++;
+        }
+    }
+}

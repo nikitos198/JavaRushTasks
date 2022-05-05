bd3f0da47c8606544ec6055e778c2a234bf2368e
nikitos198 <chupik2@gmail.com>
2022-05-05 20:02:34 +0300

    Add Solution from JavaSyntax task 0406

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0406/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0406/Solution.java
@@ -6,11 +6,11 @@ public class Solution {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         while (true) {
-            String a = console.nextLine();
-            if (a.equals("enough")){
+            String string = console.nextLine();
+            if (string.equals("enough")){
                 break;
             }
-            System.out.println(a);
+            System.out.println(string);
         }
     }
 }

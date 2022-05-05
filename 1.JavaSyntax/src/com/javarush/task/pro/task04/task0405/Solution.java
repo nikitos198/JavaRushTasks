64870001a05498e23998a41e0a0b83bb13d234f3
nikitos198 <chupik2@gmail.com>
2022-05-05 20:02:21 +0300

    Add Solution from JavaSyntax task 0405

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
@@ -4,21 +4,22 @@ package com.javarush.task.pro.task04.task0405;
 
 public class Solution {
     public static void main(String[] args) {
-        int height = 1;
-        while (height <= 10) {
+        int hight = 1;
+        while (hight <= 10){
             int width = 1;
-            while (width <= 20) {
-                if (height == 1 || height == 10) {
-                    System.out.print('Б');
+            while (width <= 20){
+                if (hight == 1 || hight == 10){
+                    System.out.print("Б");
                 } else if (width == 1 || width == 20) {
-                    System.out.print('Б');
+                    System.out.print("Б");
                 } else {
-                    System.out.print(' ');
+                    System.out.print(" ");
                 }
                 width++;
             }
             System.out.println();
-            height++;
+            hight++;
         }
+
     }
 }

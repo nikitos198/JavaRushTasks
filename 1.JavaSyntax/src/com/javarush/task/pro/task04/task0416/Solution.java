e7affe85cb8097c60345a8e521baf354eb51924d
nikitos198 <chupik2@gmail.com>
2022-05-22 18:41:58 +0300

    Add Solution from JavaSyntax task 0415

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0416/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task04.task0416;
+
+import java.util.Scanner;
+
+/* 
+Share a Coke
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int bottle = console.nextInt();
+        int people = console.nextInt();
+        System.out.println((double) bottle / people);
+    }
+}

7a350800d28d9879eb46d0fef226072416d02615
nikitos198 <chupik2@gmail.com>
2022-05-22 18:36:29 +0300

    Add Solution from JavaSyntax task 0415

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0415/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task04.task0415;
+
+import java.util.Scanner;
+
+/* 
+Площадь круга
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        double radius = console.nextDouble();
+        double pi = 3.14;
+        if (radius >= 0){
+            double s = pi * radius * radius;
+            System.out.println((int)s);
+        }
+
+    }
+}
